bc220278f91fd50fef188a029d724ba061bd1f95
Red Dragon <carsonkeller1995@hotmail.com>
2020-09-19 15:42:06 -0500

    initial setup work

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT PRIMARY KEY AUTO_INCREMENT,
+burger_name VARCHAR (30) NOT NULL,
+devoured BOOLEAN
+);
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Double Decker Surprise', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sunrise Special', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Lucky Monk', FALSE);
