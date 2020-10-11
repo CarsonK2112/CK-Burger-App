5b304385bd9fc4e0b1c4411548045739bd4e53d2
Red Dragon <carsonkeller1995@hotmail.com>
2020-10-11 16:34:02 -0500

    added burger js file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CK-Burger-App
 
+https://www.etsy.com/hk-en/listing/667321206/cheeseburger-painting-1-print
+Burger Image source
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,21 +16,21 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  sandwich.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+router.post("/api/burgers", function(req, res) {
+  sandwich.create(["name", "eaten"], [req.body.name, req.body.eaten], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   sandwich.update(
     {
-      sleepy: req.body.sleepy
+      eaten: req.body.eaten
     },
     condition,
     function(result) {
